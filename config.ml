15c520a651a80cd993be23fad08617101712413d
Romain Calascibetta <romain.calascibetta@gmail.com>
2022-10-05 23:31:30 +0200

    Re-use the happy-eyeballs instance

--- config.ml
+++ config.ml
@@ -62,17 +62,6 @@ let client =
     ~packages "Unikernel.Client"
   @@ http_client @-> time @-> pclock @-> random @-> git_client @-> job
 
-let stack = generic_stackv4v6 default_network
-let dns = generic_dns_client ~nameservers stack
-
-let git =
-  let git = git_happy_eyeballs stack dns (generic_happy_eyeballs stack dns) in
-  let tcp = tcpv4v6_of_stackv4v6 stack in
-  merge_git_clients (git_tcp tcp git)
-    (merge_git_clients
-       (git_ssh ~key:ssh_key ~authenticator:ssh_authenticator tcp git)
-       (git_http ~authenticator:tls_authenticator tcp git))
-
 let http_client =
   let connect _ modname = function
     | [ _time; _pclock; _tcpv4v6; ctx ] ->
@@ -83,20 +72,20 @@ let http_client =
   impl ~packages ~connect "Http_mirage_client.Make"
     (time @-> pclock @-> tcpv4v6 @-> git_client @-> http_client)
 
-let http_client =
-  let happy_eyeballs =
-    git_happy_eyeballs stack dns (generic_happy_eyeballs stack dns)
-  in
-  http_client $ default_time $ default_posix_clock $ tcpv4v6_of_stackv4v6 stack
-  $ happy_eyeballs
+let stack = generic_stackv4v6 default_network
+let dns = generic_dns_client ~nameservers stack
+
+let git, http =
+  let happy_eyeballs = git_happy_eyeballs stack dns (generic_happy_eyeballs stack dns) in
+  let tcp = tcpv4v6_of_stackv4v6 stack in
+  merge_git_clients (git_tcp tcp happy_eyeballs)
+    (merge_git_clients
+       (git_ssh ~key:ssh_key ~authenticator:ssh_authenticator tcp happy_eyeballs)
+       (git_http ~authenticator:tls_authenticator tcp happy_eyeballs)),
+  ( http_client $ default_time $ default_posix_clock $ tcpv4v6_of_stackv4v6 stack
+  $ happy_eyeballs )
 
 let () =
-  (* let res_dns = resolver_dns stack in
-     let conduit = conduit_direct ~tls:true stack in *)
   let job =
-    [
-      client $ http_client $ default_time $ default_posix_clock $ default_random
-      $ git;
-    ]
-  in
+    [ client $ http $ default_time $ default_posix_clock $ default_random $ git ] in
   register "friendly-unikernel" job
