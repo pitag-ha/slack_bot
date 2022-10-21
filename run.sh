43149b6d72afe0ae50674690cbeb6a97c4453848
Sonja Heinze <sonjaleaheinze@gmail.com>
2022-10-21 23:43:43 +0200

    Remove files that aren't used anymore

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ../irmin/real && git pull git@github.com:gs0510/slack_bot_data.git
-
-cd ../../slack_bot && dune exec --  ./src/slack_bot.exe
-
-cd ../irmin/real && git push origin
